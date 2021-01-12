a614a0b8d00c818c5fb3f4ef4e6cd0894bff1098
prodip2416 <prodip.sarker.cse@gmail.com>
2021-01-12 18:49:06 +0600

    lazy loading

--- lazy-loading/src/app/app-routing.module.ts
+++ lazy-loading/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user',
+    loadChildren: () => import('./users/users.module')
+      .then(mod => mod.UsersModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module')
+      .then(mod => mod.AdminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
