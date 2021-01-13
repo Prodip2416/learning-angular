e12740da3a1cc9909c9d2da1fe82d8a6a9b93b5a
prodip2416 <prodip.sarker.cse@gmail.com>
2021-01-13 14:44:50 +0600

    reactive form

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-form';
+  loginForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+  onSubmit(){
+    console.log(this.loginForm.value);
+  }
 }
--- reactive-form/src/app/app.module.ts
+++ reactive-form/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
